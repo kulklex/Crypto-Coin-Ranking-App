f950286a5d4e59a3566ab330191320f8a03b8b1f
kulklex <kulklex@gmail.com>
2023-02-14 14:40:30 +0100

    HomePage setup and some refactoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Typography, Space } from 'antd';
+import {  Link  } from 'react-router-dom'
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    return (<>
+        <Typography.Title level={5} style={{color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br/>
+            All rights reserved
+        </Typography.Title>
+        <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+        </Space>
+    </>);
 }
 
 export default Footer;
--- src/components/Outlet.js
+++ src/components/Outlet.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Footer } from 'antd/es/layout/layout'
+import Footer from './Footer'
 
 
 
@@ -8,8 +8,10 @@ export default function Outlet({children}) {
   return (
     <div className='app'>
         <div className='navbar'><Navbar/></div>
-        <div className='main'>{children}</div>
-        <div className='footer'><Footer/></div>
+        <div className='main'>
+          {children}
+          <div className='footer'><Footer/></div>
+        </div>
     </div>
   )
 }
