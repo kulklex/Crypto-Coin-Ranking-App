6881b2012dabbcf4b32a50e4e26414e500db3cd7
kulklex <kulklex@gmail.com>
2023-02-14 13:07:57 +0100

    Components and routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Outlet from './components/Outlet';
+import Exchanges from './components/Exchanges'
+import CryptoCurrencies from './components/CryptoCurrencies'
+import CryptoDetails from './components/CryptoDetails'
+import News from './components/News'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +16,10 @@ root.render(
     <Outlet>
       <Routes>
         <Route path='/' element={<App/>} />
+        <Route path='/exchanges' element={<Exchanges/>} />
+        <Route path='/cryptocurrencies' element={<CryptoCurrencies/>} />
+        <Route path='/crypto/:coinId' element={<CryptoDetails/>} />
+        <Route path='/news' element={<News/>} />
       </Routes>
     </Outlet>
     </BrowserRouter>
